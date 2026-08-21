3ef376fd75c524e5871c20b8a4e819b365cfe8d4
Kaung Htet <kaunghtet061097@gmail.com>
2026-08-22 01:08:46 +0630

    Add entrypoint script for database migrations and static files

---
+++ server/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Make migrations and migrate the database
+echo "Making migrations and migrating the database."
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the command passed by Docker
+exec "$@"
